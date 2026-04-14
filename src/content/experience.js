5da7e2c9d9f31b759bd2bc890e8c71aac7eab8a0
Louie Camacho <louiecamacho@gmail.com>
2026-04-14 16:27:21 -0500

    create experience data

---
+++ src/content/experience.js
@@ -0,0 +1,25 @@
+const experience = [
+  {
+    id: 1,
+    date: "2020—Present",
+    title: "Senior Job Title",
+    company: "Company Name",
+    bullet: ["Key achievement or responsibility demonstrating impact"],
+  },
+  {
+    id: 2,
+    date: "2020—Present",
+    title: "Senior Job Title",
+    company: "Company Name",
+    bullet: ["Key achievement or responsibility demonstrating impact"],
+  },
+  {
+    id: 3,
+    date: "2020—Present",
+    title: "Senior Job Title",
+    company: "Company Name",
+    bullet: ["Key achievement or responsibility demonstrating impact"],
+  },
+]
+
+export default experience
